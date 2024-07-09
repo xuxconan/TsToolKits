22882773cbdcfe0b5116d9d4089ee8ccee110e87
XUX <1716824725@qq.com>
2024-07-09 15:02:42 +0800

    1、增加文档注释细节 2、解决调用Pool的静态变量报错问题

--- src/Pool/index.ts
+++ src/Pool/index.ts
@@ -1,4 +1,13 @@
 import Joi from "joi";
+const pool_options_schema = Joi.object({
+  create: Joi.function().required(),
+  dispose: Joi.function(),
+  reuse: Joi.function(),
+  recycle: Joi.function(),
+  warm_up: Joi.number().integer().min(0),
+  max_count: Joi.number().integer().min(0),
+  inject_pool_funcs: Joi.boolean(),
+});
 
 type PoolObjectWithFuncs<T> = T & {
   $Recycle: (...params: any[]) => void;
@@ -22,6 +31,7 @@ type PoolOptions<T> = {
 
 /**
  * @apiGroup Pool
+ * @apiVersion 1.0.0
  * @apiDescription 对象池
  */
 export default class Pool<
@@ -29,16 +39,6 @@ export default class Pool<
   O extends PoolOptions<T>,
   INJECT_FUNCS extends O["inject_pool_funcs"],
 > {
-  private static options_schema = Joi.object({
-    create: Joi.function().required(),
-    dispose: Joi.function(),
-    reuse: Joi.function(),
-    recycle: Joi.function(),
-    warm_up: Joi.number().integer().min(0),
-    max_count: Joi.number().integer().min(0),
-    inject_pool_funcs: Joi.boolean(),
-  });
-
   private list?: Array<T>;
 
   private create: PoolObjectCreate<T>;
@@ -48,15 +48,31 @@ export default class Pool<
   private max_count?: number;
   private inject_pool_funcs?: boolean;
 
+  /**
+   * @apiGroup Pool
+   * @apiVersion 1.0.0
+   * @api {UInt} Count 【变量】Count 池内对象个数
+   **/
   Count: number;
 
   /**
-   * @api {constructor} constructor(options,...params) 创建对象池
    * @apiGroup Pool
    * @apiVersion 1.0.0
+   * @api {constructor} constructor(options,...params) 【构造】Pool 创建对象池
+   * @apiParam {Object} options 对象池配置
+   * @apiParam {Function=create(...params:Any):T} options.create 对象创建方法
+   * @apiParam {Function=dispose(obj:T,...params:Any)} [options.dispose] 对象销毁方法
+   * @apiParam {Function=reuse(obj:T,...params:Any)} [options.reuse] 对象复用方法
+   * @apiParam {Function=recycle(obj:T,...params:Any)} [options.recycle] 对象回收方法
+   * @apiParam {UInt} [options.warm_up] 预热数量
+   * @apiParam {UInt} [options.max_count] 最大数量
+   * @apiParam {Boolean} [options.inject_pool_funcs] 是否将对象池方法注入到对象中
+   * @apiParam {Array=Any} [params] 预热参数
+   * @apiSuccess (返回) {Object=Pool} pool 返回对象池实例
+   * @apiError (错误) {Error} SchemaCheckFail 参数验证错误
    */
-  constructor(options?: O, ...params: any[]) {
-    const { error, value } = Pool.options_schema.validate(options);
+  constructor(options: O, ...params: any[]) {
+    const { error, value } = pool_options_schema.validate(options);
     if (error) throw error;
 
     this.create = value.create;
@@ -90,6 +106,14 @@ export default class Pool<
     return;
   }
 
+  /**
+   * @apiGroup Pool
+   * @apiVersion 1.0.0
+   * @api {function} WarmUp(num,...params) 【方法】WarmUp 预热对象池
+   * @apiParam {UInt} num 预热数量
+   * @apiParam {Array=Any} [params] 创建/复用参数
+   * @apiError (错误) {Error} SchemaCheckFail 参数验证错误
+   */
   WarmUp(num: number, ...params: any[]) {
     const { error } = Joi.number().required().integer().min(0).validate(num);
     if (error) throw error;
@@ -108,6 +132,14 @@ export default class Pool<
     }
   }
 
+  /**
+   * @apiGroup Pool
+   * @apiVersion 1.0.0
+   * @api {function} Get(...params) 【方法】Get 获取对象
+   * @apiParam {Array=Any} [params] 创建/复用参数
+   * @apiSuccess (返回) {Object=T} obj 返回对象实例
+   * @apiSuccess (返回) {Function=$Recycle(...params:Any)} [obj.$Recycle] 对象池的回收方法（当inject_pool_funcs为true时）
+   */
   Get(
     ...params: any[]
   ): INJECT_FUNCS extends true ? PoolObjectWithFuncs<T> : T {
@@ -122,7 +154,17 @@ export default class Pool<
     return obj as INJECT_FUNCS extends true ? PoolObjectWithFuncs<T> : T;
   }
 
+  /**
+   * @apiGroup Pool
+   * @apiVersion 1.0.0
+   * @api {function} Recycle(obj,...params) 【方法】Recycle 回收对象
+   * @apiParam {Object=T} obj 对象
+   * @apiParam {Array=Any} [params] 回收/销毁参数
+   * @apiError (错误) {Error} SchemaCheckFail 参数验证错误
+   */
   Recycle(obj: PoolObject<T>, ...params: any[]) {
+    const { error } = Joi.object().required().validate(obj);
+    if (error) throw error;
     let list = this.list;
     if (!list) this.list = list = [];
     const max_count = this.max_count ?? Infinity;
@@ -135,6 +177,12 @@ export default class Pool<
     list.push(obj);
   }
 
+  /**
+   * @apiGroup Pool
+   * @apiVersion 1.0.0
+   * @api {function} Clear(...params) 【方法】Clear 清空对象池
+   * @apiParam {Array=Any} [params] 回收/销毁参数
+   */
   Clear(...params: any[]) {
     const list = this.list;
     if (!list) return;
